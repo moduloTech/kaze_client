00aa0422b9531c77747f8b4b2b3542f997055b8a
CHEVREUX Alexis <chevre_a@modulotech.fr>
2022-09-07 10:59:03 +0200

    feat: add users_request

---
+++ lib/lastbill_client/request/requests/users_request.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module KazeClient
+  # @author chevre_a@modulotech.fr
+  # List the users from the current user's company.
+  # @see KazeClient::Request
+  # @see KazeClient::Utils::FinalRequest
+  # @see KazeClient::Utils::AuthentifiedRequest
+  # @since 0.1.0
+  class UsersRequest < Utils::FinalRequest
+    include Utils::AuthentifiedRequest
+    include Utils::ListRequest
+
+    def initialize
+      super(:get, 'api/users')
+    end
+  end
+end
