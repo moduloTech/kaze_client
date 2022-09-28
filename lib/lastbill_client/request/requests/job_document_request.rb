a5ec61fabfc8651e5463e42cb571c459a28720c9
CHEVREUX Alexis <chevre_a@modulotech.fr>
2022-09-28 10:38:06 +0200

    rubocop ready

--- lib/lastbill_client/request/requests/job_document_request.rb
+++ lib/lastbill_client/request/requests/job_document_request.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 module KazeClient
-
   # @author chevre_a@modulotech.fr
   # Retrieve a job document from a job.
   # @see KazeClient::Request
@@ -9,13 +8,10 @@ module KazeClient
   # @see KazeClient::Utils::AuthentifiedRequest
   # @since 0.1.0
   class JobDocumentRequest < Utils::FinalRequest
-
     include Utils::AuthentifiedRequest
 
     def initialize(job_id, job_document_id)
       super(:get, "api/jobs/#{job_id}/documents/#{job_document_id}")
     end
-
   end
-
 end
