8c86c507b24002e9327871e4c6b096f7774f57b1
CHEVREUX Alexis <chevre_a@modulotech.fr>
2022-09-28 10:03:56 +0200

    feat: job_cancel_request

---
+++ lib/lastbill_client/request/requests/cancel_job_request.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module KazeClient
+
+  # @author chevre_a@modulotech.fr
+  # Cancel a job.
+  # @see KazeClient::Request
+  # @see KazeClient::Utils::FinalRequest
+  # @see KazeClient::Utils::AuthentifiedRequest
+  # @since 0.1.0
+  class CancelJobRequest < Utils::FinalRequest
+
+    include Utils::AuthentifiedRequest
+    include Utils::ListRequest
+
+    def initialize(job_id, cancel_reason_id)
+      super(:put, "api/jobs/#{job_id}/cancel.json")
+
+      @body = {
+        job: { cancel_reason_id: cancel_reason_id }
+      }
+    end
+
+  end
+
+end
