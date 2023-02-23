573f2ef5885f122f227dbafd797cf7abb58105dd
chevre-a-mt <99972188+chevre-a-mt@users.noreply.github.com>
2023-02-23 14:43:43 +0100

    Create create_collection_item_request.rb

---
+++ lib/kaze_client/request/requests/create_collection_item_request.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module KazeClient
+
+  # @author chevre_a@modulotech.fr
+  # Create a collection item
+  # @see KazeClient::Request
+  # @see KazeClient::Utils::FinalRequest
+  # @see KazeClient::Utils::AuthentifiedRequest
+  class CreateCollectionItemRequest < Utils::FinalRequest
+
+    include Utils::AuthentifiedRequest
+
+    # @param collection_id [String]
+    # @param payload [Hash]
+    def initialize(collection_id, payload)
+      super(:post, "api/collections/#{collection_id}/items")
+
+      @body = payload
+    end
+
+  end
+
+end
